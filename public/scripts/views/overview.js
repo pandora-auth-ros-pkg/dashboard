19d08e5c62c7b76c9737ac664b850932e7858f88
mujx <siderisk@auth.gr>
2015-07-13 01:42:01 +0300

    Add service for the rosapi
    The overview sections contains information about the available
    topics, nodes and services.

--- public/scripts/views/overview.js
+++ public/scripts/views/overview.js
@@ -5,26 +5,80 @@
  */
 
 var Backbone = require('backbone');
+var Socket = require('../ros-events.js');
 
 /**
  * Handlebars templates
  */
 
 var overviewTemplate = require('../templates/overview.hbs');
+var tableItem = require('../templates/table-item.hbs');
 
 
 var OverView = Backbone.View.extend({
 
-  el: $('#main-dashboard-content'),
+  el: '#main-dashboard-content',
+
+  nodesTable: '#nodes tbody',
+  topicsTable: '#topics tbody',
+  servicesTable: '#services tbody',
 
   overviewTemplate: overviewTemplate,
+  tableItemTemplate: tableItem,
+
+  nodes: [],
+  topics: [],
+  services: [],
+
+  events: {
+    'click #nodes-tab': 'getROSNodes',
+    'click #topics-tab': 'getROSTopics',
+    'click #services-tab': 'getROSServices'
+  },
 
   initialize: function() {
     console.log('View initialized');
+
+    Socket.on('web/rosapi/nodes', this.renderROSNodes.bind(this));
+    Socket.on('web/rosapi/topics', this.renderROSTopics.bind(this));
+    Socket.on('web/rosapi/services', this.renderROSServices.bind(this));
+  },
+
+  getROSNodes: function() {
+    Socket.emit('web/rosapi/request', 'nodes');
+  },
+
+  renderROSNodes: function(nodes) {
+    this.nodes = nodes;
+    this.$(this.nodesTable).html(this.tableItemTemplate({"items": this.nodes}));
+  },
+
+  getROSTopics: function(msg) {
+    Socket.emit('web/rosapi/request', 'topics');
+  },
+
+  renderROSTopics: function(topics) {
+    this.topics = topics;
+    this.$(this.topicsTable).html(this.tableItemTemplate(
+      {"items": this.topics}
+    ));
+  },
+
+  getROSServices: function() {
+    console.log('Listing ROS services.');
+    Socket.emit('web/rosapi/request', 'services');
+  },
+
+  renderROSServices: function(services) {
+    this.services = services;
+    this.$(this.servicesTable).html(this.tableItemTemplate(
+      {"items": this.services}
+    ));
   },
 
   render: function() {
-    this.$el.html(this.overviewTemplate({name: 'jon'}));
+    this.$el.html(this.overviewTemplate());
+    this.getROSNodes();
     return this;
   }
 
---
+++ services/rosapi.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var ROS_MASTER_IP = require('../utils/env').ROS_MASTER_IP;
+var ROS_BRIDGE_PORT = require('../utils/env').ROS_BRIDGE_PORT;
+var SERVER_PORT = require('../utils/env').SERVER_PORT;
+
+/**
+ * Load dependencies.
+ */
+
+var io = require('socket.io-client');
+var webSocket = require('ws');
+var socket = io('http://localhost:' + SERVER_PORT);
+
+
+socket.on('connect', function() {
+  console.log('Service rosapi connected to the socket.io server.');
+});
+
+socket.on('service/rosapi/request', function(req) {
+  var ws = new webSocket('ws://' + ROS_MASTER_IP + ':' + ROS_BRIDGE_PORT);
+
+  ws.on('open', function() {
+    console.log('Service rosapi connected to ROS bridge.');
+    console.log('Request: ' + req);
+    var msg = {
+      op: 'call_service',
+      service: '/rosapi/' + req,
+      args: []
+    };
+    ws.send(JSON.stringify(msg));
+  });
+
+  ws.on('message', function(data) {
+    data = JSON.parse(data.toString());
+    console.log(data);
+
+    socket.emit('service' + data.service, data.values);
+  });
+});
+
