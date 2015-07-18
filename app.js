648ce47bed68345747f5076061ce1f5721c5870c
mujx <siderisk@auth.gr>
2015-07-18 16:30:37 +0800

    Remove dependencies that need internet access

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.')));
 
 // GET home page.
 app.get('/', function(req, res, next) {
